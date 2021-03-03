7407744b900d9cb8c5414bc0203341db8f35345d
Yogesh Datir <yogdat@gmail.com>
2021-03-03 22:05:44 +0530

    Added data and selection for d3 scales

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React  from 'react';
+import { select, Selection } from 'd3-selection';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+const data = [
+  {
+    name: 'foo',
+    number: 9000
+  },
+  {
+    name: 'add',
+    number: 4500
+  },
+  {
+    name: 'gac',
+    number: 3049
+  },
+  {
+    name: 'okl',
+    number: 6700
+  },
+  {
+    name: 'yhg',
+    number: 5689
+  },
+  {
+    name: 'thg',
+    number: 2800
+  },
+]
+
 const App: React.FC = () => {
+  const ref = useRef<SVGSVGElement | null>(null)
+  const [selection, setSelection] = useState<null | Selection<SVGSVGElement | null, unknown, null, undefined>>(null)
+
+  useEffect(() => {
+    if (!selection) {
+      setSelection(select(ref.current))
+    }
+  }, [selection])
 
   return (
     <div>
