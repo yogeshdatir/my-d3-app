d0b31d2a03cdbfbc0e68538042bbfcf2eddf7d67
Yogesh Datir <yogdat@gmail.com>
2021-03-03 22:53:21 +0530

    Added x axis scale, dynamic y axis domain

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { scaleLinear } from 'd3-scale';
+import { scaleLinear, scaleBand } from 'd3-scale';
 import { select, Selection } from 'd3-selection';
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { max } from 'd3-array'
 
 const data = [
   {
     name: 'foo',
-    number: 9000
+    number: 8000
   },
   {
     name: 'add',
@@ -28,15 +29,31 @@ const data = [
     name: 'thg',
     number: 2800
   },
+  {
+    name: 'edf',
+    number: 600
+  },
+  {
+    name: 'ofh',
+    number: 6800
+  }
 ]
 
 const App: React.FC = () => {
   const ref = useRef<SVGSVGElement | null>(null)
   const [selection, setSelection] = useState<null | Selection<SVGSVGElement | null, unknown, null, undefined>>(null)
 
+  const maxValue = max(data, d => d.number)
+
   const y = scaleLinear()
-              .domain([0,10000])
-              .range([0,500])
+    .domain([0, maxValue!])
+    .range([0, 500])
+
+  const x = scaleBand()
+    .domain(data.map(d => d.name))
+    .range([0, 1000])
+    .paddingInner(0.05)
+    .paddingOuter(0.2)
 
   useEffect(() => {
     if (!selection) {
@@ -51,17 +68,24 @@ const App: React.FC = () => {
         .data(data)
         .enter()
         .append('rect')
-        .attr('width', 100)
-        .attr('x', (_, index) => index*110)
+        .attr('width', x.bandwidth)
+        .attr('x', d => {
+          const xValue = x(d.name)
+          if (xValue) {
+            return xValue
+          }
+          return null
+        })
+        // .attr('x', d => x(d.name)!)
         .attr('fill', 'orange')
         .attr('height', d => y(d.number))
     }
-  }, [selection, y])
+  }, [selection, x, y])
 
   return (
     <div>
       {/* default width of the svg is 300 */}
-      <svg ref={ref} width={800} height={500}>
+      <svg ref={ref} width={1000} height={500}>
       </svg>
     </div>
   )
