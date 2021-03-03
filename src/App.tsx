3fa47195150fb2e76a3701b3d29b77cee6bd4891
Yogesh Datir <yogdat@gmail.com>
2021-03-03 16:41:32 +0530

    Added rects for the data for which rect is not there

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,16 @@ const data = [
     height: 50,
     width: 100,
     color: 'yellow'
+  }, 
+  {
+    height: 89,
+    width: 100,
+    color: 'green'
+  }, 
+  {
+    height: 35,
+    width: 100,
+    color: 'blue'
   }
 ]
 
@@ -28,20 +38,28 @@ const App: React.FC = () => {
     if (!selection) {
       setSelection(select(ref.current))
     } else {
-      selection
+      const rects = selection
         .selectAll('rect')
         .data(data)
         .attr('width',d => d.width)
         .attr('height', d => d.height)
         .attr('fill', d => d.color)
         .attr('x', (_, index) => index * 160)
+
+        // added rects for the extra data for rects which are not in dom.
+        rects.enter()
+          .append('rect')
+          .attr('width',d => d.width)
+          .attr('height', d => d.height)
+          .attr('fill', d => d.color)
+          .attr('x', (_, index) => index * 160)
     }
   }, [selection])
 
   return (
     <div>
       {/* default width of the svg is 300 */}
-      <svg ref={ref} width='600'>
+      <svg ref={ref} width='1000'>
         <rect />
         <rect />
         <rect />
